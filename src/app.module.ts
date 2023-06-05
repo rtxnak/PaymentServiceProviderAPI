56061a5956dfe32ecc2d7c7cd7c3ecdf225e0ddc
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-06-05 20:03:53 -0300

    feat: added environment settings and UseModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.ENV === 'test' ? '.test.env' : '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
@@ -16,6 +21,7 @@ import { UserEntity } from './user/entity/user.entity';
       entities: [UserEntity],
       synchronize: process.env.ENV === 'development',
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
