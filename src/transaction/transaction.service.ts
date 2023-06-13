94b82efc1a43563fb4d5690882895cb9dda4a5d7
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-06-13 18:41:18 -0300

    feat: Added Transaction Service with create new transaction method

---
+++ src/transaction/transaction.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TransactionEntity } from './entity/transaction.entity';
+import { NewTransactionDTO } from './dto/new-transaction.dto';
+import { UserEntity } from '../user/entity/user.entity';
+
+@Injectable()
+export class TransactionService {
+  constructor(
+    @InjectRepository(TransactionEntity)
+    private transactionsRepository: Repository<TransactionEntity>,
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>,
+  ) {}
+
+  async createNewTransaction(data: NewTransactionDTO, userInfo: UserEntity) {
+    data.cardNumber = data.cardNumber.slice(-4);
+    data.customerId = userInfo.id;
+    if (
+      await this.usersRepository.exist({
+        where: {
+          id: data.companyId,
+        },
+      })
+    ) {
+      const transaction = this.transactionsRepository.create(data);
+      return this.transactionsRepository.save(transaction);
+    }
+    throw new BadRequestException('Compania não existente');
+  }
+}
