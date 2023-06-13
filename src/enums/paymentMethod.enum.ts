69904619b9d1490de7bd293aa22a8c6a0aaddcaf
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-06-13 18:38:29 -0300

    feat: Added Transaction Entity

---
+++ src/enums/paymentMethod.enum.ts
@@ -0,0 +1,4 @@
+export enum PaymentMethod {
+  debitCard = 'debit_card',
+  creditCard = 'credit_card',
+}
---
+++ src/transaction/entity/transaction.entity.ts
@@ -0,0 +1,60 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { UserEntity } from '../../user/entity/user.entity';
+import { PaymentMethod } from '../../enums/paymentMethod.enum';
+
+@Entity({
+  name: 'transactions',
+})
+export class TransactionEntity {
+  @PrimaryGeneratedColumn({
+    unsigned: true,
+  })
+  id?: number;
+  @Column('decimal', { precision: 6, scale: 2 })
+  amount: number;
+  @Column({
+    length: 127,
+  })
+  description: string;
+  @Column({
+    name: 'payment_method',
+    type: 'enum',
+    enum: PaymentMethod,
+  })
+  paymentMethod: string;
+  @Column({
+    name: 'card_number',
+    length: 4,
+  })
+  cardNumber: string;
+  @Column({
+    name: 'card_owner_name',
+    length: 127,
+  })
+  cardOwnerName: string;
+  @Column({
+    name: 'card_exp_date',
+  })
+  cardExpirationDate: string;
+  @Column({
+    name: 'card_verification_code',
+    length: 3,
+  })
+  cardVerificationCode: string;
+  @ManyToOne(() => UserEntity, (user) => user.id)
+  @JoinColumn({
+    name: 'customer_id',
+  })
+  customerId: number;
+  @ManyToOne(() => UserEntity, (user) => user.id)
+  @JoinColumn({
+    name: 'company_id',
+  })
+  companyId: number;
+}
