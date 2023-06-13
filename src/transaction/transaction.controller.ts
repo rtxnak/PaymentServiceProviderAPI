fc49ea9ab51acd9b46fe7bd9ed4f15c6757afa65
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-06-13 18:43:28 -0300

    feat: added Transction Controller and endpoint POST do create transaction on path /transaction

---
+++ src/transaction/transaction.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { TransactionService } from './transaction.service';
+import { NewTransactionDTO } from './dto/new-transaction.dto';
+import { Roles } from '../decorators/role.decorator';
+import { Role } from '../enums/role.enum';
+import { AuthGuard } from '../guards/auth.guard';
+import { RoleGuard } from '../guards/role.guard';
+import { UserInfo } from '../decorators/user-info.decorator';
+import { UserEntity } from '../user/entity/user.entity';
+
+@Roles(Role.Admin, Role.User)
+@UseGuards(AuthGuard, RoleGuard)
+@Controller('transaction')
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  @Post()
+  async create(
+    @Body() data: NewTransactionDTO,
+    @UserInfo() userInfo: UserEntity,
+  ) {
+    return this.transactionService.createNewTransaction(data, userInfo);
+  }
+}
