62d80d71f717f67374ed991e56381adde143cb6e
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-06-15 22:38:05 -0300

    feat: Added PayableEntity and PayableService as provider on transaction Module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -5,14 +5,16 @@ import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { AuthModule } from '../auth/auth.module';
 import { UserEntity } from '../user/entity/user.entity';
+import { PayableService } from '../payable/payable.service';
+import { PayableEntity } from '../payable/entity/payable.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TransactionEntity, UserEntity]),
+    TypeOrmModule.forFeature([TransactionEntity, UserEntity, PayableEntity]),
     AuthModule,
   ],
   controllers: [TransactionController],
-  providers: [TransactionService],
+  providers: [TransactionService, PayableService],
   exports: [TransactionModule],
 })
 export class TransactionModule {}
