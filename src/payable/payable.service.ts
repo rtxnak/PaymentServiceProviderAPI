d8134125b1436d4c6de6b118997153ddf6dd2cde
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-06-26 18:55:14 -0300

    feat: added listAllPayableDetailsFromCompany Method on PayableService

--- src/payable/payable.service.ts
+++ src/payable/payable.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { PaymentMethod } from '../enums/paymentMethod.enum';
 import { PayableTerm } from '../enums/payableTerm.enum';
 import { PayableTax } from '../enums/payableTax.enum';
+import { UserEntity } from '../user/entity/user.entity';
 
 @Injectable()
 export class PayableService {
@@ -39,5 +40,23 @@ export class PayableService {
       transactionId,
     });
     return this.payableRepository.save(payable);
+  }
+
+  async listAllPayableDetailsFromCompany(userInfo: UserEntity) {
+    const results = await this.payableRepository
+      .createQueryBuilder('payable')
+      .innerJoin('payable.transactionId', 'transaction')
+      .where('transaction.companyId = :id', { id: userInfo.id })
+      .getMany();
+
+    results.forEach((result) => {
+      const today = Date.now();
+      if (new Date(result.paymentDate) >= new Date(today)) {
+        result['status'] = 'waiting_funds';
+      } else {
+        result['status'] = 'paid';
+      }
+    });
+    return results;
   }
 }
