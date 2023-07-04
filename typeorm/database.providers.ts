a367cc26e077dc182dfd0e0685ced20f8c7bba29
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-07-03 21:39:14 -0300

    feat: added entities to DataSource

--- typeorm/database.providers.ts
+++ typeorm/database.providers.ts
@@ -1,5 +1,8 @@
 import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+import { UserEntity } from '../src/user/entity/user.entity';
+import { TransactionEntity } from '../src/transaction/entity/transaction.entity';
+import { PayableEntity } from '../src/payable/entity/payable.entity';
 
 dotenv.config({
   path: process.env.ENV === 'test' ? '.test.env' : '.env',
@@ -12,6 +15,7 @@ const dataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  entities: [UserEntity, TransactionEntity, PayableEntity],
   migrations: [`${__dirname}/migrations/**/*.ts`],
 });
 
